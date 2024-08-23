a377b9b6c9409c5e37c635215e2ebcbab334353c
RUben <rubencarmonaparedes@gmail.com>
2024-08-23 12:03:22 +0200

    Update main file

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,49 @@
 import { bootstrapApplication } from '@angular/platform-browser';
-import { appConfig } from './app/app.config';
-import { AppComponent } from './app/app.component';
+import { Component, OnDestroy, OnInit, signal } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { of, Subject, takeUntil } from 'rxjs';
+import { CurrencySwitcherComponent } from './app/currency-switcher/currency-switcher.component';
 
-bootstrapApplication(AppComponent, appConfig)
-  .catch((err) => console.error(err));
+@Component({
+  selector: 'my-app',
+  standalone: true,
+  imports: [CommonModule, CurrencySwitcherComponent],
+  template: `
+    <h1>Hello {{ nameToRender }}</h1>
+    <button (click)="updateValue('World!')">Update</button>
+  `,
+})
+export class App implements OnInit, OnDestroy {
+  // With Signals
+
+  // nameToRender = signal('Angular');
+
+  // <h1>Hello {{ nameToRender }}</h1>
+  // <button (click)="updateValue('World!')">Update</button>
+
+  // ---------------------------
+
+  // With Observables
+
+  name$ = of('Angular!');
+  nameToRender = '';
+  destroy$: Subject<boolean> = new Subject<boolean>();
+
+  ngOnInit() {
+    this.name$.pipe(takeUntil(this.destroy$)).subscribe((val) => {
+      this.nameToRender = val;
+    });
+  }
+
+  ngOnDestroy() {
+    this.destroy$.next(true);
+  }
+
+  updateValue(value: string) {
+    this.name$.pipe(takeUntil(this.destroy$)).subscribe(() => {
+      this.nameToRender = value;
+    });
+  }
+}
+
+bootstrapApplication(App);
